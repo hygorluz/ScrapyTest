bc52440a38b7a34f53d20076d2b6a1897d746232
Hygor Carvalho da Luz <hygor.c.luz@gmail.com>
2018-04-07 08:23:59 -0300

    Mongo db

--- wikiCrawler/spiders/articleSpider.py
+++ wikiCrawler/spiders/articleSpider.py
@@ -1,8 +1,11 @@
+import unicodedata
+from pprint import pprint
+
+from pymongo import MongoClient
 from scrapy.contrib.spiders import CrawlSpider, Rule
-from wikiCrawler.items import Article
 from scrapy.linkextractors import LinkExtractor
 
-import unicodedata
+from wikiCrawler.items import Article
 
 
 class ArticleSpider(CrawlSpider):
@@ -12,21 +15,27 @@ class ArticleSpider(CrawlSpider):
         "https://en.wikipedia.org/wiki/Python_(programming_language)"]
     rules = [Rule(LinkExtractor(allow=('(/wiki/)((?!:).)*$')),
                   callback="parse_item", follow=True)]
+    
+    client = MongoClient(port=27017)
+    db = client.wikicrawldb
+
 
     def parse_item(self, response):
         item = Article()
         title = response.xpath('//h1/text()')[0].extract()
         subtitles = response.xpath(
-            '//div[@class="toc"]/ul/li/a/span[@class="toctext"]/text()').extract()
+            '//div[@class="toc"]//li/a/span[@class="toctext"]/text()').extract()
         content = response.xpath(
             '//div[@class="mw-parser-output"]/*[not(self::table)][not(self::div)]//text()').extract()
-        print("Title is: " + title)
-        print("Subtitles : " + str(subtitles))
-        print("content is: " + str(content))
+        pprint("Title is: " + title)
+        pprint("Subtitles : " + str(subtitles))
+        pprint("content is: " + str(content))
         content_string = unicodedata.normalize("NFKD", "".join(content))
         
         item['title'] = title
         item['subtitles'] = subtitles
         item['content'] = content_string.replace(
             '"', "").replace('\\n', '\n').replace('\\u00a0', ' ')
+        result=db.reviews.insert_one(business)
+        print('Created {0} of 100 as {1}'.format(x,result.inserted_id))
         return item
